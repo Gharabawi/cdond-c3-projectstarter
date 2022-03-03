40543d9ceeb1bca1c8e7d2f86a85c78aa717ef30
Gharabawi <mada_meanyaz@yahoo.com>
2022-03-03 15:59:21 +0200

    eureka roku

--- .circleci/ansible/roles/configure-server/files/install_nvm.sh
+++ .circleci/ansible/roles/configure-server/files/install_nvm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo su -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash' &>/dev/null
-sudo su ubuntu -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash' &>/dev/null
+sudo -i -u ubuntu curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash &> /dev/null
+
