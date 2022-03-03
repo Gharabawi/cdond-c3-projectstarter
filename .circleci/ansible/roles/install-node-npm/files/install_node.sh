80b52b4e5db3612272104366cc856f7fd274ffbc
Gharabawi <mada_meanyaz@yahoo.com>
2022-03-03 16:08:28 +0200

    eureka nana

--- .circleci/ansible/roles/install-node-npm/files/install_node.sh
+++ .circleci/ansible/roles/install-node-npm/files/install_node.sh
@@ -6,7 +6,7 @@
 # sudo su ubuntu -c 'source ~/.profile; nvm install-latest-npm'
 # sudo su ubuntu -c 'source ~/.profile; nvm install 13.8.0'
 sudo -i -u ubuntu bash << EOF
-source ~/.nvm/nvm
+source ~/.nvm/nvm.sh
 nvm install-latest-npm
 nvm install 13.8.0
 EOF
