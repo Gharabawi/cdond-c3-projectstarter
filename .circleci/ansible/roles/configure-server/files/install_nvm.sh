d0372c5e2929c3a49737d02d06c50742b88c9608
Gharabawi <mada_meanyaz@yahoo.com>
2022-03-03 12:58:43 +0200

    add new ansible playbook

--- .circleci/ansible/roles/configure-server/files/install_nvm.sh
+++ .circleci/ansible/roles/configure-server/files/install_nvm.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 sudo su -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash' &>/dev/null
-sudo su -c 'source /root/.profile'
-sudo su -c 'source /root/.bashrc'
-sudo su -c 'nvm install-latest-npm'
-sudo su -c 'nvm install 13.8.0'
