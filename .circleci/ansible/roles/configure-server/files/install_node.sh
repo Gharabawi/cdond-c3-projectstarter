5d2229b700347fd876abefab45f34e942a0a8b50
Gharabawi <mada_meanyaz@yahoo.com>
2022-03-03 11:38:19 +0200

    backend deploy 10

---
+++ .circleci/ansible/roles/configure-server/files/install_node.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/ubuntu/.bashrc
+source /home/ubuntu/.profile
+nvm install 13.8.0
--- .circleci/ansible/roles/configure-server/files/install_nvm.sh
+++ .circleci/ansible/roles/configure-server/files/install_nvm.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-sh /home/ubuntu/install.sh $>/dev/null
-if [ $? -ne 0  ]
-then
-  echo "The script ran ok"
-  exit 0
-else
-  echo "The script failed" >&2
-  exit 1
-fi
+source /root/.bashrc
+source /root/.profile  
+nvm install-latest-npm
+nvm install 13.8.0
+
