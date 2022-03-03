5bb9350df4e01b246c86e986d5fb2dbad0484bbb
Gharabawi <mada_meanyaz@yahoo.com>
2022-03-03 14:12:14 +0200

    checking env var

--- .circleci/ansible/roles/install-node-npm/files/install_node.sh
+++ .circleci/ansible/roles/install-node-npm/files/install_node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo nvm install-latest-npm
-sudo nvm install 13.8.0
-nvm install-latest-npm
-nvm install 13.8.0
+sudo su -c 'nvm install-latest-npm'
+sudo su -c 'nvm install 13.8.0'
+sudo su ubuntu -c 'nvm install-latest-npm'
+sudo su ubuntu -c 'nvm install 13.8.0'
