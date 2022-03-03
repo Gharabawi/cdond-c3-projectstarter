542631263c3d6721cac04309629f6f50e6c36063
Gharabawi <mada_meanyaz@yahoo.com>
2022-03-03 20:02:19 +0200

    eureka ni-juu

--- .circleci/ansible/roles/install-node-npm/files/fix_it.sh
+++ .circleci/ansible/roles/install-node-npm/files/fix_it.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp ~container/.bashrc ~container/.bashrc_orig
-cp ~container/.bashrc ~container/.bashrc_1
-tail -n 3 ~container/.bashrc > ~container/.bashrc_1
-for i in {1..3}; do sed -i '$d' ~container/.bashrc; done
-cat ~container/.bashrc >> ~container/.bashrc_1
-mv ~container/.bashrc ~container/.bashrc_old
-mv ~container/.bashrc_1 ~container/.bashrc
+cp ~ubuntu/.bashrc ~ubuntu/.bashrc_orig
+cp ~ubuntu/.bashrc ~ubuntu/.bashrc_1
+tail -n 3 ~ubuntu/.bashrc > ~ubuntu/.bashrc_1
+for i in {1..3}; do sed -i '$d' ~ubuntu/.bashrc; done
+cat ~ubuntu/.bashrc >> ~ubuntu/.bashrc_1
+mv ~ubuntu/.bashrc ~ubuntu/.bashrc_old
+mv ~ubuntu/.bashrc_1 ~ubuntu/.bashrc
