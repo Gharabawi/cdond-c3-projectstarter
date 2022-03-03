518f50388e4bb1f92f6bc75f8235a844abaf235b
Gharabawi <mada_meanyaz@yahoo.com>
2022-03-03 12:15:24 +0200

    backend deploy fix 3

--- .circleci/ansible/roles/configure-server/files/install_nvm.sh
+++ .circleci/ansible/roles/configure-server/files/install_nvm.sh
@@ -2,5 +2,5 @@
 sudo su -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash'
 sudo su -c '. /root/.profile'
 sudo su -c '. /root/.bashrc'
-nvm install-latest-npm
-nvm install 13.8.0
+sudo su -c 'nvm install-latest-npm'
+sudo su -c 'nvm install 13.8.0'
