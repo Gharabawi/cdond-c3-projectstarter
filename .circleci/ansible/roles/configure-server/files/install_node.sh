44d79776c6b8c15418211e51d67cf95963d2a5f7
Gharabawi <mada_meanyaz@yahoo.com>
2022-03-03 13:49:06 +0200

    splitting configure server into two jobs

--- .circleci/ansible/roles/configure-server/files/install_node.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-. /home/ubuntu/.profile
-. /home/ubuntu/.bashrc
-nvm install-latest-npm
-nvm install 13.8.0
---
+++ .circleci/ansible/roles/install-node-npm/files/install_node.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo nvm install-latest-npm
+sudo nvm install 13.8.0
+nvm install-latest-npm
+nvm install 13.8.0
